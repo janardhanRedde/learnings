88e5c892b2a479bf2d2fe6110c0efb255b0d5f5a
janardhanRedde <badarijanardhanreddy912@gmail.com>
2022-09-23 19:41:14 +0530

    code

---
+++ js/lvc.js
@@ -0,0 +1,42 @@
+//[primitive types]
+// difference b/n "==" & "==="
+x= 2
+y="3"
+console.log(x==y,x===y)
+
+
+// difference b/n string
+var str = "janardhan"; // double  quotes
+var str2 = 'janardhan'; // single quotes
+
+// number
+var x = 5; // without decimal
+var y =5.6; // with decimal
+
+// biginteger (is used to store numbers)
+var biginteger = 2314555566656655656565656656565655656566565565656;
+
+// boolean (true or false)
+var a = 4
+var b = 5
+var c = 4
+console.log(a==b,a==c)
+
+// if statement ( Executes a block of code if a specified condition is true)
+// else statement ( executes a block of code if first statement is false)
+let num1 = 8
+let num2 = 18
+ 
+if(num1 > num2)
+console.log("num1 is greater")
+
+
+else
+console.log("num2 is greater")
+
+// array
+
+
+
+
+
